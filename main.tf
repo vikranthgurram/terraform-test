673e3f1ebf3c011a5b43dff3499665249c76f4b3
vikranthgurram <saradaganta2014@gmail.com>
2025-08-13 00:03:51 -0400

    added the file

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+    required_providers {
+        google = {
+            source = "hashicorp/google"
+            version = "6.15.0"
+
+        }
+    }
+}
+provider "google" {
+    credentials = "/Users/saradaganta/Downloads/new-gcp-466623-faaa6b07b2da.json"
+}
