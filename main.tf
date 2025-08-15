6bd5faa0b10a1877d1dbbd1c5971ea7d454b7391
vikranthgurram <saradaganta2014@gmail.com>
2025-08-14 23:35:13 -0400

    main tf updated

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-    credentials = "/Users/saradaganta/Downloads/new-gcp-466623-faaa6b07b2da.json"
+    credentials = file("/Users/saradaganta/Downloads/new-gcp-466623-faaa6b07b2da.json")
     project = "new-gcp-466623"
     region = "us-central1"
 }
