a2b49554f79b7cd5746f4425bd3d7085e3c3436a
vikranthgurram <saradaganta2014@gmail.com>
2025-08-14 23:27:50 -0400

    main tf updated

--- main.tf
+++ main.tf
@@ -3,10 +3,29 @@ terraform {
         google = {
             source = "hashicorp/google"
             version = "6.15.0"
-
         }
     }
 }
+
 provider "google" {
-    credentials = "/Users/saradaganta/Downloads/new-gcp-466623-faaa6b07b2da.json"
+    credentials = "${{ secrets.CREDENTIALS }}"
+    project = "new-gcp-466623"
+    region = "us-central1"
+}
+
+resource "google_compute_instance" "gcp-instance" {
+     name = "google-compute"
+     machine_type = "e2.micro"
+     zone = "us-central1-a"
+
+     boot_disk {
+        initialize_params {
+        image = "debian-cloud/debian-11"
+        size =20
+        }
+     }
+
+     network_interface {
+        network = default
+     }
 }
